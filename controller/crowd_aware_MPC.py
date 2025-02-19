f486a37d5ac92dbc2fc00b5dcb3d8a14d2c5e0ea
Yufei Zhu <yufei.zhu@oru.se>
2025-02-19 12:06:22 +0900

    update

--- controller/crowd_aware_MPC.py
+++ controller/crowd_aware_MPC.py
@@ -40,7 +40,7 @@ class CrowdAwareMPC:
         self.pref_speed =  config.getfloat('mpc_env', 'pref_speed')
         self.max_speed = config.getfloat('mpc_env', 'max_speed')
         self.max_rev_speed = config.getfloat('mpc_env', 'max_speed')
-        self.max_rot = config.getfloat('mpc_env', 'max_rot_degrees') * np.pi / 180.0
+        self.max_rot = config.getfloat('mpc_env', 'max_rot_degrees')
         self.max_l_acc = config.getfloat('mpc_env', 'max_l_acc')
         self.max_l_dcc = config.getfloat('mpc_env', 'max_l_dcc')
         
@@ -85,54 +85,72 @@ class CrowdAwareMPC:
         ## nearby human pos and vel is for current time step
         ## follow pos and vel is for all preidciton horizon
         nearby_human_pos = opti.parameter(2, self.max_humans) # [x, y] for all humans
-        nearby_human_vel = opti.parameter(2, self.max_humans) # [speed, motion_angle] for all humans
+        nearby_human_vel = opti.parameter(2, self.max_humans) # [vx, vy] for all humans
         if_follow = opti.parameter(1, 1) # 0 or 1
-        follow_pos = opti.parameter(2, self.mpc_horizon) # [x, y] for all prediction horizon
-        follow_vel = opti.parameter(2, self.mpc_horizon) # [speed, motion_angle] for all prediction horizon
+        follow_pos = opti.parameter(2, 1) # [x, y] for all prediction horizon. Set to 1 now
+        follow_vel = opti.parameter(2, 1) # [speed, motion_angle] for all prediction horizon. Set to 1 now
         
         # Cost function
-        cost = 0
+        cost_safe = 0
+        cost_smooth = 0
+
         for t in range(self.mpc_horizon):
-            
             # 1. Maintain safe distance cost
             distances = []
             for human_i in range(self.max_humans):
                 human_x = nearby_human_pos[0, human_i]
                 human_y = nearby_human_pos[1, human_i]
                 valid_human = (human_x < 1e6)
-                dist = cs.if_else(valid_human, cs.sqrt((x_var[0,t] - human_x)**2 + (x_var[1,t] - human_y)**2), 1e6)
+                # dist = cs.if_else(valid_human, cs.sqrt((x_var[0,t] - human_x)**2 + (x_var[1,t] - human_y)**2), 1e6)
+                dist = cs.if_else(valid_human, (x_var[0,t] - human_x)**2 + (x_var[1,t] - human_y)**2, 1e6)
                 distances.append(dist)
 
-            min_distance = cs.mmin(cs.vertcat(*distances))
-            cost_safe = cs.exp(cs.fmax(self.d_safe - min_distance, 0))
-            cost_safe = 0
+            # # min_distance = cs.mmin(cs.vertcat(*distances))
+            distances = cs.vertcat(*distances)
+            has_valid_human = cs.sum1(cs.if_else(distances < 1e6, 1, 0)) > 0
+
+            # Softmin to approximate minimum distance
+            alpha = 5  # Lower values make it closer to true min
+            softmin_distance = cs.sum1(distances * cs.exp(-distances / alpha)) / cs.sum1(cs.exp(-distances / alpha))
+
+            # If no valid humans, assign large safe distance
+            min_distance = cs.if_else(has_valid_human, softmin_distance, 1e6)
+
+            # Compute safety cost (only when close to humans)
+            cost_safe += cs.fmax(0, (self.d_safe - min_distance)**2) * self.w_safe
                 
-            #Crowd following cost
-            if self.use_a_omega:
-                cost_follow = if_follow * cs.sumsqr(x_var[:2, t] - follow_pos[:, t])
-                ad = mpc_utils.circdiff_casadi(x_var[3, t], follow_vel[1, t])
-                ld = cs.fabs(x_var[2, t] - follow_vel[0, t])
-                cost_follow += if_follow * cs.sqrt(ad**2 + ld**2)
-            else:
-                cost_follow = if_follow * cs.sumsqr(x_var[:2, t] - follow_pos[:, t])
-                ad = mpc_utils.circdiff_casadi(x_var[2, t], follow_vel[1, t])
-                ld = cs.fabs(u_var[0, t] - follow_vel[0, t])
-                cost_follow += if_follow * cs.sqrt(ad**2 + ld**2)
+            # #Crowd following cost, along all the time_horizon
+            # if self.use_a_omega:
+            #     cost_follow = if_follow * cs.sumsqr(x_var[:2, t] - follow_pos[:, t])
+            #     ad = mpc_utils.circdiff_casadi(x_var[3, t], follow_vel[1, t])
+            #     ld = cs.fabs(x_var[2, t] - follow_vel[0, t])
+            #     cost_follow += if_follow * cs.sqrt(ad**2 + ld**2)
+            # else:
+            #     cost_follow = if_follow * cs.sumsqr(x_var[:2, t] - follow_pos[:, t])
+            #     ad = mpc_utils.circdiff_casadi(x_var[2, t], follow_vel[1, t])
+            #     ld = cs.fabs(u_var[0, t] - follow_vel[0, t])
+            #     cost_follow += if_follow * cs.sqrt(ad**2 + ld**2)
         
             # 3. Smooth control
-            cost_smooth = 0
             if t > 0:
-                cost_smooth = cs.sumsqr(u_var[:, t] - u_var[:, t - 1])
-
-            cost += self.w_safe * cost_safe + self.w_smooth * cost_smooth + self.w_follow * cost_follow
+                cost_smooth += cs.sumsqr(u_var[:, t] - u_var[:, t - 1]) * self.w_smooth
             
-        # #Crowd following cost
-        # cost_follow = if_follow * cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, self.mpc_horizon-1])
-        # ad = mpc_utils.circdiff_casadi(x_var[3, self.mpc_horizon-1], follow_vel[1, self.mpc_horizon-1])
-        # ld = cs.fabs(x_var[2, self.mpc_horizon-1] - follow_vel[0, self.mpc_horizon-1])
-        # cost_follow += if_follow * cs.sqrt(ad**2 + ld**2)
+        #Crowd following cost, only check the last time step
+        if self.use_a_omega:
+            ad = mpc_utils.circdiff_casadi(x_var[3, self.mpc_horizon-1], follow_vel[1, 0])
+            ld = (x_var[2, self.mpc_horizon-1] - follow_vel[0, 0])
+        else:
+            ad = mpc_utils.circdiff_casadi(x_var[2, self.mpc_horizon-1], follow_vel[1, 0])
+            ld = (u_var[0, self.mpc_horizon-1] - follow_vel[0, 0])
+            
+        # cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, 0]) + cs.sqrt(ad**2 + ld**2))
+        # cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, 0])) 
+        cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, 0]) + ad**2 + ld**2)        
+        cost_goal = self.w_goal * cs.sumsqr(x_var[:2, self.mpc_horizon-1] - target)
         
-        cost += self.w_goal * cs.sumsqr(x_var[:2, self.mpc_horizon-1] - target)
+        # cost += self.w_safe * cost_safe + self.w_smooth * cost_smooth
+        
+        cost = cost_goal * (1-if_follow) + cost_follow * if_follow + cost_safe + cost_smooth
         
         opti.minimize(cost)
         
@@ -153,11 +171,22 @@ class CrowdAwareMPC:
         opti.subject_to(x_var[:, 0] == x_init)
         
         # Solver setup
+        # opts = {
+        #     "ipopt.print_level": ipopt_print_level, 
+        #     "print_time": 0,
+        #     "ipopt.max_iter": self.max_mp_steps,}
+        
         opts = {
-            "ipopt.print_level": ipopt_print_level, 
+            "ipopt.print_level": ipopt_print_level,
             "print_time": 0,
-            "ipopt.max_iter": self.max_mp_steps,}
+            "ipopt.max_iter": 5000,  # Adjustable based on performance
+            "ipopt.acceptable_iter": 10,  # Stop if at least 10 "acceptable" iterations are found
+            "ipopt.acceptable_tol": 1e-2,  # Allow a relaxed tolerance
+            "ipopt.acceptable_obj_change_tol": 1e-2  # Stop if improvement is small
+        }
         opti.solver("ipopt", opts)
+
+        
         
         # Store solver attributes
         self.opti = opti
@@ -174,7 +203,7 @@ class CrowdAwareMPC:
             "follow_vel": follow_vel,
         }
 
-    def get_action(self, current_state, target, nearby_human_pos, nearby_human_vel, follow_pos, follow_vel):
+    def get_action(self, current_state, target, nearby_human_state, follow_state):
         """Solves the MPC optimization and returns the next control action."""
         opti = self.opti
 
@@ -183,28 +212,21 @@ class CrowdAwareMPC:
         opti.set_value(self.opti_dict["target"], target.reshape(-1,1))
 
         # Set crowd status
-        if nearby_human_pos is not None and nearby_human_vel is not None:
-            num_humans = nearby_human_pos.shape[0]
-            padded_pos = np.full((self.max_humans, 2), 1e6)
-            padded_vel = np.full((self.max_humans, 2), 1e6)
-            padded_pos[:num_humans, :] = nearby_human_pos
-            padded_vel[:num_humans, :] = nearby_human_vel
-            
-            opti.set_value(self.opti_dict["nearby_human_pos"], padded_pos.T)
-            opti.set_value(self.opti_dict["nearby_human_vel"], padded_vel.T)
-        else:
-            opti.set_value(self.opti_dict["nearby_human_pos"], np.full((2, self.max_humans), 1e6))
-            opti.set_value(self.opti_dict["nearby_human_vel"], np.full((2, self.max_humans), 1e6))
+        nearby_human_pos = nearby_human_state[:, :2]
+        nearby_human_vel = nearby_human_state[:, 2:]
+        opti.set_value(self.opti_dict["nearby_human_pos"], nearby_human_pos.T)
+        opti.set_value(self.opti_dict["nearby_human_vel"], nearby_human_vel.T)
         
-        if follow_pos is not None and follow_vel is not None:
-            opti.set_value(self.opti_dict["if_follow"], 1)
-            opti.set_value(self.opti_dict["follow_pos"], follow_pos.T)
-            opti.set_value(self.opti_dict["follow_vel"], follow_vel.T)
-        else:
-            opti.set_value(self.opti_dict["if_follow"], 0)
-            opti.set_value(self.opti_dict["follow_pos"], np.full((2, self.mpc_horizon), 1e6))  # Ignore cost
-            opti.set_value(self.opti_dict["follow_vel"], np.full((2, self.mpc_horizon), 1e6))  # Ignore cost
-
+        follow_pos = follow_state[:, :2]
+        follow_vel = follow_state[:, 2:]
+        opti.set_value(self.opti_dict["follow_pos"], follow_pos.T)
+        opti.set_value(self.opti_dict["follow_vel"], follow_vel.T)
+        
+        ## check if follow_state is the padded value
+        if_follow = 0
+        if np.sum(follow_vel) < 1e6:
+            if_follow = 1
+        opti.set_value(self.opti_dict["if_follow"], if_follow)
 
         # Solve the MPC problem
         try:
@@ -212,21 +234,123 @@ class CrowdAwareMPC:
         except:
             print("----------------------------------")
             print("Solver failed. Debugging values:")
-            x_var = self.opti_dict["x_var"]
-            u_var = self.opti_dict["u_var"]
-            print("x_var (state trajectory):", opti.debug.value(x_var))
-            print("u_var (control inputs):", opti.debug.value(u_var))
+            x_var = opti.debug.value(self.opti_dict["x_var"])
+            u_var = opti.debug.value(self.opti_dict["u_var"])
+            print("x_var (state trajectory):", x_var)
+            print("u_var (control inputs):", u_var)
+            
+            follow_vel = opti.debug.value(self.opti_dict["follow_vel"])
+            follow_pos = opti.debug.value(self.opti_dict["follow_pos"])
+            
+            print("follow_vel: ", follow_vel)
+            print("follow_pos: ", follow_pos)
+            
+            nearby_human_pos = opti.debug.value(self.opti_dict["nearby_human_pos"])
+            nearby_human_vel = opti.debug.value(self.opti_dict["nearby_human_vel"])
+            
+            cost_safe = self.get_cost_safe(x_var, nearby_human_pos)
+            cost_smooth = self.get_cost_smooth(u_var)
+            cost_follow = self.get_cost_follow(x_var, u_var, follow_pos, follow_vel, if_follow)
+            cost_goal = self.get_cost_goal(x_var, target)
+
+            print("cost_safe: ", cost_safe)
+            print("cost_smooth: ", cost_smooth)
+            print("cost_follow: ", cost_follow)
+            print("cost_goal: ", cost_goal)
+            
             solver_stats = opti.stats()  # Get solver statistics
             print("Solver return status:", solver_stats["return_status"])
             print("Solver iterations:", solver_stats["iter_count"])
             print("Solver objective value:", solver_stats["iter_stats"]["obj"])
             print("----------------------------------")
 
-            
-
             raise  # Re-raise error after debugging
-
+        
+        ################ Extract costs after solving #########################
+        x_var = sol.value(self.opti_dict["x_var"])
+        u_var = sol.value(self.opti_dict["u_var"])
+        cost = sol.value(self.opti_dict["opti"].f)
+        follow_vel = sol.value(self.opti_dict["follow_vel"])
+        follow_pos = sol.value(self.opti_dict["follow_pos"])
+        if_follow = sol.value(self.opti_dict["if_follow"])
+        nearby_human_pos = sol.value(self.opti_dict["nearby_human_pos"])
+        nearby_human_vel = sol.value(self.opti_dict["nearby_human_vel"])
+        
+        cost_safe = self.get_cost_safe(x_var, nearby_human_pos)
+        cost_smooth = self.get_cost_smooth(u_var)
+        cost_follow = self.get_cost_follow(x_var, u_var, follow_pos, follow_vel, if_follow)
+        cost_goal = self.get_cost_goal(x_var, target)
+        
+        print("cost_safe: ", cost_safe)
+        print("cost_smooth: ", cost_smooth)
+        print("cost_follow: ", cost_follow)
+        print("cost_goal: ", cost_goal)
+        print("total cost: ", cost)
+        
+        print("follow weight: ", self.w_follow, " goal weight: ", self.w_goal)
+        print("if_follow: ", sol.value(if_follow))
+        ######################################################################
+        
         # Extract first control action
         u_opt = sol.value(self.opti_dict["u_var"][:, 0])
 
         return u_opt, sol.value(self.opti_dict["x_var"])
+    
+
+    def get_cost_safe(self, x_var, nearby_human_pos):
+        cost_safe = 0
+        for t in range(self.mpc_horizon):
+            # 1. Maintain safe distance cost
+            distances = []
+            for human_i in range(self.max_humans):
+                human_x = nearby_human_pos[0, human_i]
+                human_y = nearby_human_pos[1, human_i]
+                valid_human = (human_x < 1e6)
+                if human_x < 1e6:
+                    dist = cs.if_else(valid_human, (x_var[0,t] - human_x)**2 + (x_var[1,t] - human_y)**2, 1e6)
+                    distances.append(dist)
+
+            distances = cs.vertcat(*distances)
+            has_valid_human = cs.sum1(cs.if_else(distances < 1e6, 1, 0)) > 0
+
+            # Softmin to approximate minimum distance
+            alpha = 5  # Lower values make it closer to true min
+            softmin_distance = cs.sum1(distances * cs.exp(-distances / alpha)) / cs.sum1(cs.exp(-distances / alpha))
+
+            # If no valid humans, assign large safe distance
+            min_distance = cs.if_else(has_valid_human, softmin_distance, 1e6)
+
+            # Compute safety cost (only when close to humans)
+            cost_safe += cs.fmax(0, (self.d_safe - min_distance)**2) * self.w_safe
+            
+            print("distances", distances)
+            print("min_distance: ", min_distance)
+            
+        return cost_safe
+    
+    def get_cost_smooth(self, u_var):
+        cost_smooth = 0
+        for t in range(self.mpc_horizon):
+            cost_smooth += cs.sumsqr(u_var[:, t] - u_var[:, t - 1])
+            
+        return cost_smooth * self.w_smooth
+    
+    def get_cost_follow(self, x_var, u_var, follow_pos, follow_vel, if_follow):
+        if self.use_a_omega:
+            ad = mpc_utils.circdiff_casadi(x_var[3, self.mpc_horizon-1], follow_vel[1])
+            ld = (x_var[2, self.mpc_horizon-1] - follow_vel[0])
+        else:
+            ad = mpc_utils.circdiff_casadi(x_var[2, self.mpc_horizon-1], follow_vel[1])
+            ld = (u_var[0, self.mpc_horizon-1] - follow_vel[0])
+            
+        # cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, 0]) + cs.sqrt(ad**2 + ld**2))
+        # cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos[:, 0])) 
+        cost_follow = self.w_follow * (cs.sumsqr(x_var[:2, self.mpc_horizon-1] - follow_pos) + ad**2 + ld**2)
+        cost_follow = if_follow * cost_follow
+        
+        return cost_follow
+    
+    def get_cost_goal(self, x_var, target):
+        cost_goal = self.w_goal * cs.sumsqr(x_var[:2, self.mpc_horizon-1] - target)
+        
+        return cost_goal
--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # python3 main.py --group --record --react --animate
-# python3 main.py --group --record --animate
+python3 main.py --group --record --animate
 
-for atc_file_num in {1..20..1}
-do
-    python3 check_scenarios.py --group --record --animate --dset-file datasets_atc.yaml --atc-file-num "$atc_file_num"
-done
+# for atc_file_num in {1..20..1}
+# do
+#     python3 check_scenarios.py --group --record --animate --dset-file datasets_atc.yaml --atc-file-num "$atc_file_num"
+# done
 
 
