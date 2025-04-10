fbef7a81c6bf246e556e85086acfa35f7e877770
shihminyang <ethansmin.yang@gmail.com>
2025-04-10 17:15:19 +0200

    [feat] parallel training

--- main_paral.py
+++ main_paral.py
@@ -11,7 +11,8 @@ from config import get_args, check_args
 from sim.simulator import Simulator
 from sim.simulator_parallel import SimulatorGym
 from sim.mpc.ped_nopred_mpc import PedNoPredMPC
-from controller.crowd_aware_MPC import CrowdAwareMPC
+# from controller.crowd_aware_MPC import CrowdAwareMPC
+from controller.group_linear_mpc import GroupLinearMPC
 from controller import mpc_utils
 from obs_data_parser import ObsDataParser
 
@@ -21,41 +22,8 @@ from torch.utils.tensorboard import SummaryWriter
 from rl.rl_agent import SAC
 from rl.trainer import ContinuousSACTrainer
 from rl.utils import load_config
+from main import set_random_seed
 #### -----------------------------------
-
-
-#### RL model
-def preprocess_rl_obs(obs, current_state, robot_vx, robot_vy, goal_pos):
-    """ img_obs: A Numpy array with (max_human, 4) in float32.
-        Process it into torch tensor with (bs, max_humna*4) in float32.
-    """
-    obs = obs.copy()
-    current_state = current_state.copy()
-    current_pos = current_state[:2].reshape(1, -1)
-    obs[:, :2] = obs[:, :2] - current_pos
-    obs[obs > 1e4] = 0
-
-    obs[:, 2] = obs[:, 2] - robot_vx
-    obs[:, 3] = obs[:, 3] - robot_vy
-
-    goal_pos = np.array(goal_pos).reshape(1, -1)
-    goal_pos = goal_pos - current_pos
-    obs = obs.reshape(1, -1)
-    obs = np.concatenate([goal_pos, obs], axis=1)
-    return obs
-
-#### -----------------------------------
-
-
-def set_random_seed(seed):
-    seed = seed if seed >= 0 else random.randint(0, 2**32)
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
-    return seed
 
 
 def make_env(env_id, seed, kwargs, wrapper_list=[], capture_video=False, run_name="video"):
@@ -90,6 +58,9 @@ if __name__ == "__main__":
     logging.basicConfig(level=level, handlers=[stdout_handler, file_handler],
 						format='%(asctime)s, %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
     logger = logging.getLogger(__name__)
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    logging.getLogger('numba').setLevel(logging.WARNING)
+    logging.getLogger('byteflow').setLevel(logging.WARNING)
 
     check_args(args, logger)
 
@@ -113,14 +84,15 @@ if __name__ == "__main__":
     # data_file = "ucy_1"
     # data_file = "ucy_2"
     # data_file = "eth_0"
-    data_file = "all"
+    # data_file = "all"
+    data_file = "eth0_left_to_right"
 
     # Load configs
     mpc_config = mpc_utils.parse_config_file("controller/crowd_mpc.config")
     rl_config = load_config("rl_config.yaml")
 
     # Create simulator
-    env_id = "grid_maze_env"
+    env_id = "crowd_nav_env"
     env_config = {
         "args": args,
         "case_fpath": f"data/{data_file}.json",
@@ -140,8 +112,10 @@ if __name__ == "__main__":
 
     env_list = [make_env(env_id, args.seed + i, kwargs, wrapper_list)
                 for i in range(args.num_envs)]
-    sim = gym.vector.AsyncVectorEnv(env_list)
+    sim = gym.vector.AsyncVectorEnv(
+        env_list, autoreset_mode=gym.vector.AutoresetMode.SAME_STEP)
 
+    # sim = Simulator(args, f"data/{data_file}.json", logger)       # Put in simulator_parallel.py
     output_dir = sim.get_attr("output_dir")[0]
     os.makedirs(os.path.join(output_dir, "evas"), exist_ok=True)
     eva_res_dir = os.path.join(output_dir, "evas", f"{data_file}_{args.exp_name}.csv")
@@ -164,14 +138,14 @@ if __name__ == "__main__":
     rl_agent = SAC(rl_config["state_shape"], rl_config["action_shape"],
                    rl_config["latent_dim"], device)
     rl_trainer = ContinuousSACTrainer(
-        rl_agent, result_dir, rl_config, args.num_envs)
+        rl_agent, result_dir, rl_config, sim.num_envs)
     train_info = {}
 
     tb_writer = SummaryWriter(f"{rl_config['result_dir']}/logs/{args.exp_name}/{int(time.time())}")
     logger.info(f"RL result directory: {result_dir}")
     ########################################################################
     # sim.case_id_list.sort()
-    # np.random.shuffle(sim.case_id_list)
+    # np.random.shuffle(sim.case_id_list)       # Put in simulator_parallel.py
 
     # mpc_config = mpc_utils.parse_config_file("controller/crowd_mpc.config")
     obs_data_parser = ObsDataParser(mpc_config, args)
@@ -181,17 +155,17 @@ if __name__ == "__main__":
     max_follow_pos_delta = (mpc_config.getint('mpc_env', 'mpc_horizon') *
                             mpc_config.getfloat('mpc_env', 'max_speed'))
 
-    obs, _ = sim.reset()
-    mpcs = [CrowdAwareMPC(mpc_config, args.use_a_omega, args.differential)
-            for _ in range(args.num_envs)]
+    obs, sim_info = sim.reset()
+
+    ###### MPC initialization ######
+    # mpcs = [CrowdAwareMPC(mpc_config, args.use_a_omega, args.differential)
+    #         for _ in range(args.num_envs)]
+    mpcs = [GroupLinearMPC(mpc_config, args, logger) for _ in range(sim.num_envs)]
+    ################################
 
     # max_steps = len(sim.get_attr("case_id_list")[0])
     max_steps = 2_000_000
     for _ in range(max_steps):
-        # if case_id <= 42:
-        #     continue
-        # done = False
-        # print("step:", _)
         current_state, target, nearby_human_state, robot_vx, robot_vy, rl_obs = \
             obs["current_state"], obs["target"], obs["nearby_human_state"], obs["robot_vx"], obs["robot_vy"], obs["rl_obs"]
 
@@ -202,22 +176,25 @@ if __name__ == "__main__":
                 torch.FloatTensor(rl_obs).to(device))
             rl_actions = rl_actions.cpu().detach().numpy()
         else:
-            rl_actions = rl_agent.random_actions(args.num_envs)
+            rl_actions = rl_agent.random_actions(sim.num_envs)
 
         # Rescale actions. rl_actions is (1, 4): pos_x, pos_y, vel_x, vel_y, and they are all relative values to the robot, both pos and vel
         follow_pos = rl_actions[:, :2].copy()
-        follow_vel = rl_actions[:, 2:].copy()
+        # follow_vel = rl_actions[:, 2:].copy()
 
         ## Now rerange the follow_pos and follow_vel
         follow_pos = follow_pos * max_follow_pos_delta     # Since max_follow_pos_delta > 0
         # revert the relative pos to global pos
         follow_pos = follow_pos + current_state[:, :2]
 
-        follow_vel = (follow_vel + 1) * (mpcs[0].max_speed + mpcs[0].max_rev_speed) / 2 - mpcs[0].max_rev_speed     # Since max_rev_speed > 0
-        follow_vel = follow_vel + np.concatenate([robot_vx, robot_vy], axis=1)
+        # follow_vel = (follow_vel + 1) * (mpcs[0].max_speed + mpcs[0].max_rev_speed) / 2 - mpcs[0].max_rev_speed     # Since max_rev_speed > 0
+        # follow_vel = follow_vel + np.concatenate([robot_vx, robot_vy], axis=1)
 
-        follow_speed = np.linalg.norm(follow_vel, axis=1)
-        follow_motion_angle = np.mod(np.arctan2(follow_vel[:, 1], follow_vel[:, 0]), 2 * np.pi)
+        # follow_speed = np.linalg.norm(follow_vel, axis=1)
+        # follow_motion_angle = np.mod(np.arctan2(follow_vel[:, 1], follow_vel[:, 0]), 2 * np.pi)
+        # TODO: Check these shape
+        follow_speed = np.zeros_like(follow_pos[:, 0])
+        follow_motion_angle = np.zeros_like(follow_pos[:, 0])
 
         follow_state = np.concatenate([
             follow_pos, follow_speed.reshape(-1, 1),
@@ -225,19 +202,24 @@ if __name__ == "__main__":
         #########################################################
 
         actions_mpc = []
-        for env_idx in range(args.num_envs):
-            action_mpc, _ = mpcs[env_idx].get_action(
-                current_state[env_idx], target[env_idx],
-                nearby_human_state[env_idx], follow_state[env_idx:env_idx+1])
+        obs_dicts = sim_info["obs_dict"]
+        for env_idx in range(sim.num_envs):
+            obs_dict = obs_dicts[env_idx][0]
+            action_mpc = mpcs[env_idx].get_action(
+                obs_dict,
+                target[env_idx],
+                follow_state[env_idx:env_idx+1])
             actions_mpc.append(action_mpc)
         actions_mpc = np.stack(actions_mpc, axis=0)
+        actions_mpc = np.ones([2, 2])
         next_obs, reward, terminated, truncated, sim_info = sim.step(actions_mpc)
 
         #################### RL model output the follow_pos #############################
+        dones = terminated | truncated
         real_next_rl_obs = next_obs["rl_obs"].copy()
-        for idx, truncs in enumerate(truncated):
-            if truncs and "final_observation" in sim_info:
-                real_next_rl_obs[idx] = sim_info["final_observation"][idx]
+        for env_idx, done in enumerate(dones):
+            if done:
+                real_next_rl_obs[env_idx] = sim_info["final_obs"][env_idx]["rl_obs"]
 
         rl_trainer.add_to_buffer(rl_obs, real_next_rl_obs, rl_actions, reward, terminated, [{}])
         rl_trainer.update_episode_info(reward)
@@ -246,18 +228,16 @@ if __name__ == "__main__":
             rl_trainer.reporter["reach_goal_reward_dense"].append(sim_info["reach_goal_reward_dense"])
             rl_trainer.reporter["group_matching_reward"].append(sim_info["group_matching_reward"])
 
-        if terminated.any() or truncated.any():
-            dones = terminated | truncated
+        if dones.any():
             rl_infos = {"is_success": terminated}
             rl_trainer.record_episode_info(dones, rl_infos)
-            for env_idx in range(args.num_envs):
+            for env_idx in range(sim.num_envs):
                 if dones[env_idx]:
-                    mpcs[env_idx] = CrowdAwareMPC(mpc_config, args.use_a_omega, args.differential)
+                    mpcs[env_idx] = GroupLinearMPC(mpc_config, args, logger)
 
         obs = next_obs
 
         if rl_trainer.is_train_model():
-            # print("Training the RL model")
             train_info = rl_trainer.train_model()
 
         rl_trainer.report_progress(tb_writer, train_info)
